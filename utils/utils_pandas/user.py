2d4425f1f94bffb858731a21ef8d960cfba2c381
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-27 13:07:44 +0330

    added user manager

---
+++ utils/utils_pandas/user.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pandas import DataFrame
+from random_token import random_token_generator
+from datetime import datetime, timedelta
+
+df = pd.read_csv('./data/user.csv')
+
+def search_user(email: str) -> DataFrame:
+    entry_exists = df[df['email'] == email]
+    if not entry_exists.empty:
+        return entry_exists
+    return None
+
+def check_password(user: DataFrame, password: str) -> bool:
+    return user['password'].values[0] == password
+
+def change_token(user: DataFrame):
+    user_index = df[df['email'] == user['email'].values[0]].index[0]
+    df.at[user_index, "token"] = random_token_generator(size=8)
+    df.at[user_index, "token_creation_time"] = datetime.now()
+    df.to_csv('./data/user.csv', index=False)
+
+def insert_user(email: str, password: str, token: str, token_time: datetime):
+    new_entry = {'email': email, 'password': password, 'token': token, 'token_creation_time': token_time}
+    global df
+    df = df.append(new_entry, ignore_index=True)
+    df.to_csv('./data/user.csv', index=False)
+
+def check_token_expiration(user: DataFrame, input_token: str, expire_minutes: int) -> bool:
+    if input_token == user['token'].values[0]:
+        token_creation_time = pd.to_datetime(user['token_creation_time'].values[0])
+        expiration_time = token_creation_time + timedelta(minutes=expire_minutes)
+        return datetime.now() > expiration_time
+    return False
